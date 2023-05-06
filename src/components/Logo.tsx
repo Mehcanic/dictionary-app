6937cdd69cff8165c231bab36a2470411dff2c98
Lukasz Bialy <bialy.lukasz0@gmail.com>
2023-05-06 13:11:53 +0100

    working on logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 import LogoSvg from '../assets/logo.svg'
 
+// write this component so it will accept different svgs as params
+
+
 function Logo() {
   return (
     <img src={LogoSvg} alt="" />
